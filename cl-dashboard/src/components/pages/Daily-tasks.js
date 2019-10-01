cb5669957a4f9c2d7eb6e51bdb3ac4df94d3852b
Jerin Raisa <jerin.raisa@crowdlinker.com>
2019-10-01 12:16:56 -0400

    updates

--- cl-dashboard/src/components/pages/Daily-tasks.js
+++ cl-dashboard/src/components/pages/Daily-tasks.js
@@ -1,15 +1,27 @@
-import React from 'react'; 
-import List from '../list.js';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Header from './Title'
+import List from '../list.js'
 
-class DailyTasks extends React.Component{    
+const HomePageWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const TitleMain = styled.h1`
+`;
+
+class DailyTasks extends Component{
     render(){
         return(
-            <div>
-                <h1>Tasks</h1>
-                <List/>
-            </div>
+
+            <HomePageWrapper>
+            
+                <Header>Daily Tasks</Header>
+                <List></List>
+
+            </HomePageWrapper>
         )
     }
 }
-
 export default DailyTasks;
