a131ace8179495a3d07d52c019c380d42b98af8a
Lynn Kim <lynnkim2000@gmail.com>
2019-10-02 12:58:04 -0400

    fixes

--- cl-dashboard/src/components/Navigation.jsx
+++ cl-dashboard/src/components/Navigation.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React from "react";
 import { Route, Link, BrowserRouter as Router } from "react-router-dom";
-import { Pages } from './pages/pages.js'
-import styled from 'styled-components'
-import List from './list.js'
+import { Pages } from "./pages/pages.js";
+import styled from "styled-components";
+import List from "./List.jsx";
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    background: #FEFEFE;
+  display: flex;
+  align-items: center;
+  background: #fefefe;
 `;
 
 const TodayDate = styled.div`
-    display: flex;
-    justify-content: flex-end;
-    color: #A7B0BC;
-    text-align: right;
-    font-family: Arial, Helvetica, sans-serif;
-    font-weight: 900;
-    // margin: 15px;
-    padding-right: 10px;
-    padding-top: 10px;
-    background: #FEFEFE;
+  display: flex;
+  justify-content: flex-end;
+  color: #a7b0bc;
+  text-align: right;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 900;
+  // margin: 15px;
+  padding-right: 10px;
+  padding-top: 10px;
+  background: #fefefe;
 `;
 
 const NavWrapper = styled.div`
@@ -39,61 +39,62 @@ const NavWrapper = styled.div`
 `;
 
 // Date settings
-var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+};
 let d = new Date();
 
-function PageLink (props) {
-    return (
-        <div>
-        <a
+function PageLink(props) {
+  return (
+    <div>
+      <a
         rel="noopener noreferrer"
         href={props.site}
         target={props.target}
-        style={{paddingLeft: 13, textDecoration: 'none'}}
-        >
+        style={{ paddingLeft: 13, textDecoration: "none" }}
+      >
         <i className="navlink ">{props.tag}</i>
-        </a>
-        </div>
-    ); 
+      </a>
+    </div>
+  );
 }
 
-
-class Navigation extends React.Component{
-
-    render(){
-        return(
-            <>
-            <TodayDate>{d.toLocaleDateString("en-US", options)}</TodayDate> 
-            <Container>
-                <div>
-                {/* Routing for navigation bar  */}
-                <Router>
-                    {Pages.map((page) => {
-                        return (
-                            <Route
-                            key={page.page}
-                            path={page.path}
-                            component={page.page}
-                            />
-                        )
-                    })}
-                </Router>
-                <NavWrapper>
-                    {/* Nav side bar  */}
-                    <PageLink site={"/overview"} tag={"Overview"}/>
-                    <PageLink site={"/daily-tasks"} tag={"Daily Tasks"}/>
-                    <PageLink site={"/self-evaluation"} tag={"Self Evaluation"}/>
-                    <PageLink site={"/goals"} tag={"Goals"}/>
-                    <PageLink site={"/resources"} tag={"Resources"}/>
-                </NavWrapper>
-                </div>
-                {/* <List></List> */}
-            </Container>
-            </>
-        )
-    }
+class Navigation extends React.Component {
+  render() {
+    return (
+      <>
+        <TodayDate>{d.toLocaleDateString("en-US", options)}</TodayDate>
+        <Container>
+          <div>
+            {/* Routing for navigation bar  */}
+            <Router>
+              {Pages.map(page => {
+                return (
+                  <Route
+                    key={page.page}
+                    path={page.path}
+                    component={page.page}
+                  />
+                );
+              })}
+            </Router>
+            <NavWrapper>
+              {/* Nav side bar  */}
+              <PageLink site={"/overview"} tag={"Overview"} />
+              <PageLink site={"/daily-tasks"} tag={"Daily Tasks"} />
+              <PageLink site={"/self-evaluation"} tag={"Self Evaluation"} />
+              <PageLink site={"/goals"} tag={"Goals"} />
+              <PageLink site={"/resources"} tag={"Resources"} />
+            </NavWrapper>
+          </div>
+          {/* <List></List> */}
+        </Container>
+      </>
+    );
+  }
 }
 
-
-
-export default Navigation
+export default Navigation;
