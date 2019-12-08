5345e53395ff4f687ca707c773ea0a7ce6bca036
Jerin Raisa <jerin.raisa@crowdlinker.com>
2019-12-08 16:04:12 -0500

    adding archive

--- src/components/ArchiveSelector/index.jsx
+++ src/components/ArchiveSelector/index.jsx
@@ -16,14 +16,14 @@ class Archive extends Component {
     year: ""
   };
 
-  // handleChange = (property, value) => {
-  //   this.setState({ ...state, ...{ [property]: value } });
-  //   console.log(e);
-  // };
+  handleChange = (property, value) => {
+    this.setState({ ...state, ...{ [property]: value } });
+    console.log(e);
+  };
 
-  // select(e) {
-  //   alert(e.target.value);
-  // }
+  select(e) {
+    alert(e.target.value);
+  }
   render() {
     return (
       <ArchiveContainer>
@@ -31,21 +31,21 @@ class Archive extends Component {
           <Dropdown
             className="test2"
             options={Month}
-            // onChange={e => this.handleChange("month", e.target.value)}
+            onChange={e => this.handleChange("month", e.target.value)}
             placeholder="Month"
           />
         </div>
         <Dropdown
           className="test1"
           options={Day}
-          // onChange={e => this.handleChange("day", e.target.value)}
+          onChange={e => this.handleChange("day", e.target.value)}
           placeholder="Day"
         />
 
         <Dropdown
           className="test3"
           options={Year}
-          // onChange={e => this.handleChange("year", e.target.value)}
+          onChange={e => this.handleChange("year", e.target.value)}
           placeholder="Year"
         />
       </ArchiveContainer>
